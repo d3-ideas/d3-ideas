e415b6efd31c1ff3aae07f44227a7ffb8b42d9b3
Anthony Sheetz <sheetzam@earthlink.net>
2014-02-14 01:59:31 +0000

    start of writing a pin

--- WebServer/app.js
+++ WebServer/app.js
@@ -33,6 +33,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.post('/pin', routes.pin);
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('          / \\');
