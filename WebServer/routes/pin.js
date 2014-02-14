7949483fb59aab4976e79e0300a5c2471b74aa4b
Anthony Sheetz <sheetzam@earthlink.net>
2014-02-14 02:50:39 +0000

    more bug fixin

---
+++ WebServer/routes/pin.js
@@ -0,0 +1,26 @@
+exports.pin = function (req, res){
+	var http = require('http');
+	var options = {
+		hostname: 'localhost',
+		port: 3001,
+		path: '/pins',
+		method: 'POST'
+	};
+
+	var ourPost = http.request(options, function(res) {
+		console.log('STATUS: ' + res.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(res.headers));
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			console.log('BODY: ' + chunk);
+		});
+	});
+
+	ourPost.on('error', function(e) {
+		console.log('problem with request: ' + e.message);
+	});
+
+	// write data to request body
+	ourPost.write(JSON.stringify({'location':[req.lat,req.long],'username':'sheetzam','pintime':new Date()}));
+	ourPost.end();
+};
