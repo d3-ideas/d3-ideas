5e03fd90d75d5596750365b31c7aa00c337fd388
Kai Clements <ztm@earthlink.net>
2014-02-22 00:36:15 -0500

    Application is validated and stores the _id in the pin.
    Note: your pin drop will fail if the application does not exist in the
    'apps' collection.  There is a line of code you can uncomment to insert
    it if you need it.

--- Storage/routes/pins.js
+++ Storage/routes/pins.js
@@ -18,46 +18,40 @@ exports.addPin = function(db){
         var username = req.body.username;
         var pintime = new Date();
         var application = req.body.application;
-        
+ 
+
         if(!gjv.isPoint(location)){
             res.send({'error':'The pin location was invalid.'});
             return;
         }
         
-        if(!checkApp(location, db)){
-            res.send({'error':'The application was invalid.'});
-            return;
-        }
+        var apps = db.get('apps');
+
+// Uncomment the next line to insert the application pin (only need to do this once)        
+//        apps.insert({'Application':application});
         
-        var collection = db.get('pins');
-    
-        collection.insert({
-            'Location' : location,
-            'UserName' : username,
-            'PinTime' : pintime,
-            'Application' : application
-        }, function (err,doc){
-            if (err) {
-                res.send({'error':'An error has occurred adding your pin'});
-            } else {
-                console.log('Successfully added pin');
-                res.send(doc); //You have pinned your location
+        apps.findOne({'Application':application}, function(err,foundapp){
+            if(!foundapp) {
+                res.send({'error':'The application was invalid.'});
             }
+            else{
+                application = foundapp._id;
+                var collection = db.get('pins');
+    
+                collection.insert({
+                    'Location' : location,
+                    'UserName' : username,
+                    'PinTime' : pintime,
+                    'Application' : application
+                }, function (err,doc){
+                    if (err) {
+                        res.send({'error':'An error has occurred adding your pin'});
+                    } else {
+                        console.log('Successfully added pin');
+                        res.send(doc); //You have pinned your location
+                    }
+                });
+            } 
         });
     }
 };
-
-	
-checkApp = function(findapp, db){
-    return function(req, res){
-        var apps = db.get('apps');
- 
-        apps.insert({'application':findapp});
- 
-        apps.findOne({application:findapp}, function(err,foundapp){
-            console.log('foundapp: ' + foundapp.application);
-            if(foundapp.application == findapp) res.send(true);
-            else res.send(false);
-        });        
-    }    
-}
