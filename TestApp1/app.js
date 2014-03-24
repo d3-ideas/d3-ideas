ac73b5b4f732348435d6bf75fc4daf021a935deb
sheetzam <sheetzam@earthlink.net>
2014-03-23 21:37:05 -0400

    now with more structure

--- TestApp1/app.js
+++ TestApp1/app.js
@@ -8,21 +8,44 @@ app.get('/', function (req, res) {
     res.sendfile(__dirname + '/names.html');
 });
 
-function getNames (callback){
-    var theReturn = '<h1>Here are names</h1>';
-    callback(theReturn);
+function getNames (count, callback){
+    console.log(count);
+    var theReturn = '<h1>Here are names</h1>' + count;
+    if (true){ //db call for example
+        //it all worked well
+        callback(undefined, theReturn);
+    }
+    else {
+        var theError='something went wrong';
+        callback(theError, false);
+    }
 };
 
 app.get('/giveMeNames', function(req, res){
-    getNames (function(theData){
+    console.log(req.query);
+    getNames (req.query.count, function(error, theData){
+        //checking for an error should be implemented.
         console.log('got http request for names');
         res.send(theData);
     });
 });
 
 io.sockets.on('connection', function (socket) {
+    socket.on('giveMeAge', function (data){
+        console.log(data);
+        getAge(data.count, function(error, theData){
+            if (typeof error !== 'undefined'){
+                socket.emit('namesHere', {'error': error.error});
+            }
+            else {
+                console.log('got socket request for names');
+                socket.emit('namesHere', {'data': theData});
+            }
+        });
+    });
     socket.on('giveMeNames', function (data){
-        getNames(function(theData){
+        console.log(data);
+        getNames(data.count, function(theData){
             console.log('got socket request for names');
             socket.emit('namesHere', {'data': theData});
         });
