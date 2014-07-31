a2cbd2e25251bdddc1e04e9d96f3ca2b517ce1b2
Rob Vignerot <robvignerot@gmail.com>
2014-07-30 21:39:32 -0400

    Move Port to 3000

--- WebServer/app.js
+++ WebServer/app.js
@@ -24,7 +24,7 @@ var app = express();
 var MongoStore = require('connect-mongo')(express);
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
